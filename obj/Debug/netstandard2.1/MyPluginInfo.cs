b5bbbcf08e94e7a92555f7581c22b365614b7be7
CraftyC7 <78454072+CraftyC7@users.noreply.github.com>
2025-05-27 14:47:43 -0400

    Initial commit

---
+++ obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace LunarConfig
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "Crafty.LunarConfig";
+        public const string PLUGIN_NAME = "LunarConfig";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
